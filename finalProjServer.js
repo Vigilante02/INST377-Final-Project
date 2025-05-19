4412c3f3af313f112b3903438369fbb2c59e0071
Vigilante02 <rcborak@outlook.com>
2025-05-18 20:44:29 -0400

    Final Project Edit

--- finalProjServer.js
+++ finalProjServer.js
@@ -53,4 +53,4 @@ app.post('/api/favorites', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`🚀 Server listening on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
